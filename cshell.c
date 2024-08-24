a2eaf167bd6ea41db7d8d6f81ac9f42001b266e3
Yeo Xin Ee Araki <84427221+Arakii0@users.noreply.github.com>
2024-08-25 03:34:23 +0800

    Implemented exit function and error handling

--- cshell.c
+++ cshell.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 int main(){
 
@@ -10,6 +12,14 @@ int main(){
         // Wait for user input
         char input[100];
         fgets(input, 100, stdin);
+
+        input[strlen(input) - 1] = '\0'; // Remove newline character
+
+        if(!strcmp(input, "exit")) {
+            exit(0);
+        }
+
+        printf("%s: command not found\n", input);
     }
     
     return 0;
