21c6924c634b42027d4d87e9c7ded43c97c6a0c3
Yeo Xin Ee Araki <84427221+Arakii0@users.noreply.github.com>
2024-08-26 00:03:19 +0800

    Finished changing of directories and pwd command

--- cshell.c
+++ cshell.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <Windows.h>
+#include <Shlobj.h>
 
 int main(){
 
@@ -19,22 +21,23 @@ int main(){
         // Remove newline character
         input[strlen(input) - 1] = '\0'; 
 
-        // Exit Function
+        // Exit Command
         if (!strcmp(input, "exit")) {
             exit(0);
         }
 
-        // Echo Function
+        // Echo Command
         if (!strncmp(input, "echo", strlen("echo"))) {
             printf("%s\n", input + 5);
             continue;
         }
 
-    
-        // Type function, checks if command is found in shell
+        // Type Command, checks if command is found in shell
         if (!strncmp(input, "type", strlen("type"))) {
             bool found = false;
             for (int i = 0; i < sizeof(functions) / sizeof(functions[0]); i++) {
+
+                // Checks to see if command is in the list of functions above
                 if (!strcmp(input + 5, functions[i])) {
                     printf("%s is a shell builtin\n", input +5);
                     found = true;
@@ -45,6 +48,37 @@ int main(){
                 printf("%s: not found\n", input + 5);
             continue;
         }
+
+        // Print Current Directory Command
+        if (!strcmp(input, "pwd")) {
+            char path[100];
+            GetCurrentDirectory(100 ,path);
+            printf("%s\n", path);
+            continue;
+        }
+
+        // Changes the directory
+        if (!strncmp(input, "cd", strlen("cd"))) {
+
+            // Changes to user's home directory
+            if (!strcmp(input + 3, "~")){
+                WCHAR homeDir[100];
+                if (SUCCEEDED(SHGetFolderPathW(NULL, CSIDL_PROFILE, NULL, 0, homeDir))) {
+                    SetCurrentDirectoryW(homeDir);
+                }
+                else {
+                    printf("cd %s: Home Directory not found\n", input + 3);
+                }
+                continue;
+            }
+
+            if (SetCurrentDirectory(input + 3) == 0) {
+                printf("cd %s: No such file or directory\n", input + 3);
+            }
+            continue;
+        }
+
+
 
         // If command is not found in shell
         printf("%s: command not found\n", input);
